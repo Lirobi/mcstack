5e517aed58e074575ff63859d9e178e3d1d9d21c
Lilian Bischung <lilian.bischung@gmail.com>
2025-01-24 17:11:35 +0100

    ajout fonctionalités + color

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 function getNumInMCStacks(num) {
+    let stackSize = 64;
+    if (document.getElementById("checkbox").checked) {
+        stackSize = 16;
+    }
     let res = "";
-    let stackCount = Math.round(num / 64);
-    let rest = num % 64;
+    let stackCount = Math.round(num / stackSize);
+    let rest = num % stackSize;
     res = stackCount + " stacks et " + rest + " items";
     return res;
 }
 
 document.getElementById("input").addEventListener("input", function() {
     document.getElementById("result").innerHTML = getNumInMCStacks(this.value);
+});
+
+document.getElementById("checkbox").addEventListener("change", function() {
+    document.getElementById("result").innerHTML = getNumInMCStacks(document.getElementById("input").value);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("input").value = 75;
+    document.getElementById("result").innerHTML = getNumInMCStacks(document.getElementById("input").value);
 });
